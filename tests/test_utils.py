fc8dd781c0849dc20b865b0edb025ce47581a49f
ttran-tech <duy@ttran.tech>
2025-03-05 19:40:05 +0000

    Refactor: disable print_banner test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,11 +4,10 @@ import unittest
 
 class TestUtils(unittest.TestCase):
     """Unit test for utility functions defined in utils.py"""
-    def test_print_banner(self):
-        print_banner()
+    # def test_print_banner(self):
+    #     print_banner()
 
     def test_binary_to_decimal(self):
-        """Test binary_to_decimal() function"""
         self.assertEqual(binary_to_decimal("11000000"), 192, "Binary and decimal are not equal")
         self.assertEqual(binary_to_decimal("11111111"), 255, "Binary and decimal are not equal")        
         self.assertEqual(binary_to_decimal("10101000"), 168, "Binary and decimal are not equal")
