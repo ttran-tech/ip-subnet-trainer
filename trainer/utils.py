f12bcb2dde5ca181469f47fc5c618dd64f6b8293
Tan <duytantran2710@gmail.com>
2025-03-04 21:56:40 -0800

    Feat: add print_banner function

--- trainer/utils.py
+++ trainer/utils.py
@@ -1,6 +1,19 @@
 #/bin/usr/python
 from math import pow
 
+def print_banner():
+    print()
+    print("┌─────────────────────────────────────────────┐")
+    print("│     _______    ____     __            __    │")
+    print("│    /  _/ _ \\  / __/_ __/ /  ___  ___ / /_   │")
+    print("│   _/ // ___/ _\\ \\/ // / _ \\/ _ \\/ -_) __/   │")
+    print("│  /___/_/    /___/\\_,_/_.__/_//_/\\__/\\__/    │")
+    print("│  /_  __/______ _(_)__  ___ ____             │")
+    print("│    / / / __/ _ `/ / _ \\/ -_) __/            │")
+    print("│   /_/ /_/  \\_,_/_/_//_/\\__/_/               │")
+    print("│                                 ttran.tech  │")
+    print("└─────────────────────────────────────────────┘")
+    print()
 
 def binary_to_decimal(binary: str) -> int:
     """Convert binary to decimal number"""
