3204d2c85ac3d100ffaee16052b9b9c5686c05e1
ttran-tech <duy@ttran.tech>
2025-03-05 23:41:58 +0000

    Feat: implement calcualte_subnet_mas function [WIP]

---
+++ src/calculator.py
@@ -0,0 +1,29 @@
+# This files contains the functions that calculate and generate the answer
+# based on the IP and CIDR from the generator.
+
+def calculate_subnet_mask(cird:str) -> str:
+    """Calculate subnet mask from a given CIDR"""
+    binary_subnet = {1:'00000000', 2:'00000000', 3:'00000000', 4:'00000000'}
+    octet = 1   # first octet
+    full_octet = int(int(cird) / 8)
+    extra_bit = int(int(cird) % 8)
+
+    print(f"binary_subnet before {binary_subnet}")
+
+    for i in range(full_octet):
+        binary_subnet[octet] = '11111111'
+        octet += 1
+
+    if extra_bit > 0:
+        for i in range(extra_bit):
+            binary_subnet[octet] = binary_subnet[octet][:i] + '1' + binary_subnet[octet][i+1:]
+
+    print(f"binary_subnet after {binary_subnet}")
+    print()
+
+if __name__ == "__main__":
+    subnet = calculate_subnet_mask(8)
+    subnet = calculate_subnet_mask(12)
+    subnet = calculate_subnet_mask(20)
+    subnet = calculate_subnet_mask(28)
+    subnet = calculate_subnet_mask(30)
