4cc9202c76c3fea2d3ffff073d6dc06fbbe4481f
ttran-tech <duy@ttran.tech>
2025-03-05 18:38:18 +0000

    Test: centralize all the test scripts into run_test.

---
+++ run_test.py
@@ -0,0 +1,8 @@
+# This file contains the main source code to run
+# all the unit test in /test folder.
+import unittest
+from src.utils import *
+from tests.test_utils import TestUtils
+
+if __name__ == "__main__":
+    unittest.main()
