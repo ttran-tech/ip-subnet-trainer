3cae97d0be40e7ed84850c72d0c4e1f113d7619b
ttran-tech <duy@ttran.tech>
2025-03-05 19:40:35 +0000

    Feat: implement and test generate_ip

---
+++ src/generator.py
@@ -0,0 +1,20 @@
+# This file contains the functions for generating the IP Address and CIRD suffix
+import random
+
+
+def _generate_first_octet(start:int, end:int, exclude:int) -> int:
+    """Generate the first octet of the IP address, but excludes 127 (loopback) """
+    octet = random.randint(start, end)
+    if octet == exclude:
+        return (_generate_first_octet(start, end, exclude))
+    return octet
+
+
+def generate_ip() -> str:
+    """
+    Generate a random IP address from the following classes:
+        Class A (1-126)
+        Class B (128-191)
+        Class C (192-223)
+    """
+    return f"{ _generate_first_octet(1, 223, 127) }.{ random.randint(1, 255) }.{ random.randint(1, 255) }.{ random.randint(1, 255) }"
