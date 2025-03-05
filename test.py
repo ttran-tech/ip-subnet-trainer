5c58f577c0c177fa803b0035027b8f54c242edec
Tan <duytantran2710@gmail.com>
2025-03-04 21:28:17 -0800

    Test: add unit test for binary_to_decimal function.

---
+++ test.py
@@ -0,0 +1,14 @@
+#/bin/usr/python
+import unittest
+
+from trainer.utils import *
+
+class TestUtils(unittest.TestCase):
+    def test_binary_to_decimal(self):
+        self.assertEqual(binary_to_decimal("11000000"), 192, "Binary and decimal are not equal")
+        self.assertEqual(binary_to_decimal("11111111"), 255, "Binary and decimal are not equal")        
+        self.assertEqual(binary_to_decimal("10101000"), 168, "Binary and decimal are not equal")
+        
+
+if __name__ == "__main__":
+    unittest.main()
