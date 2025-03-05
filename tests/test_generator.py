4e33f3e747745e7c94b91da8e7bdd1cf1785fa40
ttran-tech <duy@ttran.tech>
2025-03-05 21:22:05 +0000

    Test: add unit test for generate_cidr

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -6,7 +6,9 @@ class TestGenerator(unittest.TestCase):
     def test_generate_ip(self):
         ip = generate_ip()
         pattern = r'^(\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3})$'
-        compiled_p = re.compile(pattern=pattern)
-
-        # print(f"Tesing IP: {ip} | { compiled_p.match(ip) }")
         self.assertTrue(bool(re.match(pattern, ip)), "IP Address not match.")
+
+    def test_generate_cidr(self):
+        cidr = generate_cidr()
+        pattern = r'^/(\d){1,2}$'
+        self.assertTrue(bool(re.match(pattern, cidr)), "Invalid CIDR suffix.")
